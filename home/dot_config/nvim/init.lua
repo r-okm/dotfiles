3cd83fed6b792449829c465b47144cf700f01653
r-okm <r.okm.00113@gmail.com>
2022-12-08 12:50:05 +0900

    [vim] win32yank をフルパスで指定

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -25,16 +25,17 @@ for k, v in pairs(set_options) do
 end
 
 -- wsl でホストとクリップボードを共有
-if vim.fn.has 'wsl' == 1 then
+local win32yank = '/mnt/c/scoop/shims/win32yank.exe'
+if vim.fn.has('wsl') == 1 and vim.fn.executable(win32yank) == 1 then
   vim.g.clipboard = {
     name = "win32yank-wsl",
     copy = {
-      ["+"] = "win32yank -i --crlf",
-      ["*"] = "win32yank -i --crlf",
+      ["+"] = win32yank .. " -i --crlf",
+      ["*"] = win32yank .. " -i --crlf",
     },
     paste = {
-      ["+"] = "win32yank -o --lf",
-      ["*"] = "win32yank -o --lf",
+      ["+"] = win32yank .. " -o --lf",
+      ["*"] = win32yank .. " -o --lf",
     },
     cache_enable = 0,
   }
