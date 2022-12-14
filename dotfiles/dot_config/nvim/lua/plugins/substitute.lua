975702c4b1311c6ea4807e7b61d3429c33ebfb88
r-okm <r.okm.00113@gmail.com>
2022-12-14 23:12:22 +0900

    [vim] 不要な文字列指定をやめる

--- dotfiles/dot_config/nvim/lua/plugins/substitute.lua
+++ dotfiles/dot_config/nvim/lua/plugins/substitute.lua
@@ -1,4 +1,4 @@
 return function()
   local keymap = require('utils.setKeymap').keymap
-  keymap('nx', ',', '<cmd>lua require("substitute").operator()<cr>')
+  keymap('nx', ',', function() require("substitute").operator() end)
 end
