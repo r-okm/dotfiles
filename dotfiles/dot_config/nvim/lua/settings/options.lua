8ce4fc2580912346a0bac6ce7c205d3941e49e3f
r-okm <r.okm.00113@gmail.com>
2022-12-15 22:18:04 +0900

    [vim] indent 設定

--- dotfiles/dot_config/nvim/lua/settings/options.lua
+++ dotfiles/dot_config/nvim/lua/settings/options.lua
@@ -1,10 +1,23 @@
--- set options
 local o = vim.o
+local opt = vim.opt
 
 o.whichwrap = 'b,s,h,l,<,>,~,[,]'
 o.smartcase = true
 o.ignorecase = true
 o.clipboard = 'unnamedplus'
+
+-- not vscode
+if not vim.g.vscode then
+  o.number = true
+  o.relativenumber = true
+  o.termgouicolors = true
+  o.list = true
+  opt.listchars = { tab = '»-', trail = '●' }
+  o.expandtab = true
+  local indent = 2
+  o.shiftwidth = indent
+  o.softtabstop = indent
+end
 
 -- wsl でホストとクリップボードを共有
 local win32yank = '/mnt/c/scoop/shims/win32yank.exe'
@@ -21,11 +34,4 @@ if vim.fn.has('wsl') and vim.fn.executable(win32yank) then
     },
     cache_enable = 0,
   }
-end
-
--- vscode
-if not vim.g.vscode then
-  o.number = true
-  o.relativenumber = true
-  o.termgouicolors = true
 end
