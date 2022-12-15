1d35e04a7c45370a4700badf17bd663b8e7e714b
r-okm <r.okm.00113@gmail.com>
2022-12-15 19:29:59 +0900

    [vim] buffer を閉じた後にサイドバーをフォーカスしない

--- dotfiles/dot_config/nvim/lua/settings/keymaps.lua
+++ dotfiles/dot_config/nvim/lua/settings/keymaps.lua
@@ -35,5 +35,5 @@ else
   keymap('n', '<C-h>', ':<C-u>bprev<CR>')
   keymap('n', '<C-l>', ':<C-u>bnext<CR>')
   keymap('n', '<Leader>s', ':<C-u>w<CR>')
-  keymap('n', '<Leader>w', ':<C-u>bw<CR>')
+  keymap('n', '<Leader>w', ':<C-u>bw<CR>:<C-u>bp<CR>')
 end
