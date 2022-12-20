fcb82c9ab6bdf1d19b86155b7317e4484d718461
r-okm <r.okm.00113@gmail.com>
2022-12-21 07:39:01 +0900

    [vim] nvim-cmp マッピング変更

--- dotfiles/dot_config/nvim/lua/plugins/completion/_nvim-cmp.lua
+++ dotfiles/dot_config/nvim/lua/plugins/completion/_nvim-cmp.lua
@@ -15,7 +15,9 @@ return function()
     mapping = cmp.mapping.preset.insert({
       ["<C-p>"] = cmp.mapping.select_prev_item(),
       ["<C-n>"] = cmp.mapping.select_next_item(),
-      ['<C-l>'] = cmp.mapping.complete(),
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(),
+      ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
       ["<CR>"] = cmp.mapping.confirm { select = true },
     }),
