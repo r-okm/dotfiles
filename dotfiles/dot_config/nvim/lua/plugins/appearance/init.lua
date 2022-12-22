7833db9895064dc07f7f53e0bfb2902a0eb50a6a
r-okm <r.okm.00113@gmail.com>
2022-12-22 11:47:46 +0900

    [vim] telescope project_files 関数追加

--- dotfiles/dot_config/nvim/lua/plugins/appearance/init.lua
+++ dotfiles/dot_config/nvim/lua/plugins/appearance/init.lua
@@ -54,12 +54,18 @@ return function(use)
       end
 
       local telescope = require('telescope')
+      local builtin = require('telescope.builtin')
+
       telescope.load_extension('fzf')
       telescope.load_extension('neoclip')
 
-      local builtin = require('telescope.builtin')
+      telescope.project_files = function()
+        local ok = pcall(builtin.git_files)
+        if not ok then builtin.find_files() end
+      end
+
       local keymap = require('utils.setKeymap').keymap
-      keymap('n', '<C-p>', builtin.git_files)
+      keymap('n', '<C-p>', telescope.project_files)
       keymap('n', '<C-g>', builtin.git_status)
       keymap('n', '<C-f>', builtin.live_grep)
       keymap('n', 'gp', ':<C-u>Telescope neoclip<CR>')
