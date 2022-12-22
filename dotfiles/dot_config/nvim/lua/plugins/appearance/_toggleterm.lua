91ce81c81158280a31c0250d64a6d2e4a3597ad5
r-okm <r.okm.00113@gmail.com>
2022-12-22 15:31:06 +0900

    [vim] terminal 設定追加

--- dotfiles/dot_config/nvim/lua/plugins/appearance/_toggleterm.lua
+++ dotfiles/dot_config/nvim/lua/plugins/appearance/_toggleterm.lua
@@ -3,6 +3,7 @@ return function()
     return
   end
 
+  -- lazygit
   local Terminal = require('toggleterm.terminal').Terminal
   local lazygit = Terminal:new({
     cmd = 'lazygit',
@@ -16,4 +17,20 @@ return function()
 
   local keymap = require('utils.setKeymap').keymap
   keymap('n', '<C-k><C-g>', '<cmd>lua LazygitToggle()<CR>')
+
+  -- toggleterm
+  require('toggleterm').setup({
+    direction = 'float',
+    -- size = 100,
+  })
+  keymap('n', '<C-n>', ':ToggleTerm<CR>')
+
+  function _G.set_terminal_keymaps()
+    local opts = { buffer = 0 }
+    vim.keymap.set('t', '<CR>', [[<CR>]], opts)
+    vim.keymap.set('t', '<ESC>', [[<C-\><C-n>]], opts)
+    vim.keymap.set('t', '<C-n>', [[<C-\><C-n>:ToggleTerm<CR>]], opts)
+  end
+
+  vim.cmd('autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()')
 end
