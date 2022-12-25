0048b0b4523b6dc6e7b78240e7e3abe70928a198
r-okm <r.okm.00113@gmail.com>
2022-12-25 20:40:45 +0900

    [vim] telescope の設定ファイルを分離

---
+++ dotfiles/dot_config/nvim/lua/plugins/appearance/_telescope.lua
@@ -0,0 +1,51 @@
+local M = {}
+
+M.config = function()
+  if vim.g.vscode then return end
+
+  local telescope = require('telescope')
+  local builtin = require('telescope.builtin')
+
+  telescope.load_extension('fzf')
+  telescope.load_extension('neoclip')
+
+  telescope.project_files = function()
+    local ok = pcall(builtin.git_files)
+    if not ok then builtin.find_files() end
+  end
+
+  local keymap = require('utils.setKeymap').keymap
+  keymap('n', '<C-p>', telescope.project_files)
+  keymap('n', '<C-g>', builtin.git_status)
+  keymap('n', '<C-f>', builtin.live_grep)
+  keymap('n', 'gp', ':<C-u>Telescope neoclip<CR>')
+end
+
+M.setup = function()
+  local actions = require('telescope.actions')
+  local telescope = require('telescope')
+
+  telescope.setup {
+    defaults = {
+      mappings = {
+        n = {
+          ['q'] = actions.close
+        }
+      }
+    },
+    pickers = {
+      find_files = { hidden = true, },
+      git_files = { show_untracked = true, },
+    },
+    extensions = {
+      fzf = {
+        fuzzy = true,
+        override_generic_sorter = true,
+        override_file_sorter = true,
+        case_mode = 'smart_case',
+      },
+    },
+  }
+end
+
+return M
--- dotfiles/dot_config/nvim/lua/plugins/appearance/init.lua
+++ dotfiles/dot_config/nvim/lua/plugins/appearance/init.lua
@@ -54,52 +54,8 @@ return function(use)
     cond = function()
       return not vim.g.vscode
     end,
-    setup = function()
-      if vim.g.vscode then return end
-
-      local telescope = require('telescope')
-      local builtin = require('telescope.builtin')
-
-      telescope.load_extension('fzf')
-      telescope.load_extension('neoclip')
-
-      telescope.project_files = function()
-        local ok = pcall(builtin.git_files)
-        if not ok then builtin.find_files() end
-      end
-
-      local keymap = require('utils.setKeymap').keymap
-      keymap('n', '<C-p>', telescope.project_files)
-      keymap('n', '<C-g>', builtin.git_status)
-      keymap('n', '<C-f>', builtin.live_grep)
-      keymap('n', 'gp', ':<C-u>Telescope neoclip<CR>')
-    end,
-    config = function()
-      local actions = require('telescope.actions')
-      local telescope = require('telescope')
-
-      telescope.setup {
-        defaults = {
-          mappings = {
-            n = {
-              ['q'] = actions.close
-            }
-          }
-        },
-        pickers = {
-          find_files = { hidden = true, },
-          git_files = { show_untracked = true, },
-        },
-        extensions = {
-          fzf = {
-            fuzzy = true,
-            override_generic_sorter = true,
-            override_file_sorter = true,
-            case_mode = 'smart_case',
-          },
-        },
-      }
-    end,
+    setup = require('plugins.appearance._telescope').setup,
+    config = require('plugins.appearance._telescope').config,
   }
   use {
     'aznhe21/actions-preview.nvim',
