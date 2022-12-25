2db3e44ad3e2ba63aaf5ee5e0aa705e45f385bc8
r-okm <r.okm.00113@gmail.com>
2022-12-25 09:39:58 +0900

    [vim] devicon を opt に変更

--- dotfiles/dot_config/nvim/lua/plugins/appearance/init.lua
+++ dotfiles/dot_config/nvim/lua/plugins/appearance/init.lua
@@ -138,7 +138,7 @@ return function(use)
   use {
     'nvim-tree/nvim-tree.lua',
     requires = {
-      { 'nvim-tree/nvim-web-devicons' },
+      { 'nvim-tree/nvim-web-devicons', opt = true },
     },
     wants = { 'nvim-web-devicons' },
     cond = function()
