5d5aec9a760db89b8c8cefc598d15e6fa1c4406a
r-okm <r.okm.00113@gmail.com>
2022-12-28 16:58:12 +0900

    [vim] jsonls のフォーマットを無効

--- dotfiles/dot_config/nvim/lua/lsp/servers/jsonls.lua
+++ dotfiles/dot_config/nvim/lua/lsp/servers/jsonls.lua
@@ -15,9 +15,11 @@ local M = {
     }
   },
   capabilities = require("cmp_nvim_lsp").default_capabilities(),
-  on_attach = function(_, bufnr)
+  on_attach = function(client, bufnr)
     local lspKeymapToBuffer = require("lsp.utils.lspKeymapToBuffer")
     lspKeymapToBuffer(bufnr)
+
+    client.server_capabilities.documentFormattingProvider = false
   end,
 }
 
