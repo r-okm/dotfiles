1adeb715177ba2300be6d4555b1e5a43a460ea16
r-okm <r.okm.00113@gmail.com>
2022-12-29 09:57:35 +0900

    [vim] noh %s コマンドをキー割当

--- dotfiles/dot_config/nvim/lua/settings/keymaps.lua
+++ dotfiles/dot_config/nvim/lua/settings/keymaps.lua
@@ -1,40 +1,37 @@
-local util = require('utils.setKeymap')
+local util = require("utils.setKeymap")
 local keymap = util.keymap
 local keymapVsc = util.keymapVsc
 local keymapVscVisual = util.keymapVscVisual
 
 -- map prefix
-keymap('', '<Space>', '')
+keymap("", "<Space>", "")
 
-keymap('nxo', 'gh', '^')
-keymap('nxo', 'gl', '$')
-keymap('n', '<C-k><C-n>', ':<C-u>noh<CR>')
+keymap("nxo", "gh", "^")
+keymap("nxo", "gl", "$")
+keymap("n", "<C-k><C-n>", ":<C-u>noh<CR>")
+keymap("n", "<C-k><C-s>", ":<C-u><CR>:%s/", { silent = false })
 
 -- vscode
 if vim.g.vscode then
   -- buffer
-  keymapVsc('n', '<Space>s', 'workbench.action.files.save')
-  keymapVsc('n', '<Space>w', 'workbench.action.closeActiveEditor')
-  keymapVsc('n', '<Space>t', 'workbench.action.reopenClosedEditor')
+  keymapVsc("n", "<Space>s", "workbench.action.files.save")
+  keymapVsc("n", "<Space>w", "workbench.action.closeActiveEditor")
+  keymapVsc("n", "<Space>t", "workbench.action.reopenClosedEditor")
   -- filer
-  keymapVsc('n', '<Space>c', 'workbench.action.closeSidebar')
-  keymapVsc('n', '<Space>e', 'workbench.view.explorer')
-  -- jumpToBracket
-  keymapVsc('nx', 'gb', 'editor.action.jumpToBracket')
+  keymapVsc("n", "<Space>c", "workbench.action.closeSidebar")
+  keymapVsc("n", "<Space>e", "workbench.view.explorer")
   -- insert new line in normal mode
-  keymapVsc('n', '<Cr>', 'editor.action.insertLineAfter')
+  keymapVsc("n", "<Cr>", "editor.action.insertLineAfter")
   -- global search
-  keymapVsc('n', '#', 'workbench.action.findInFiles', '{ "query": expand("<cword>")}')
-  keymapVscVisual('x', '#', 'workbench.action.findInFiles', '{ "query": expand("<cword>")}')
-  -- git
-  keymapVsc('n', 'zs', 'multiCommand.gitStatusWindow')
-  keymapVsc('n', 'zd', 'git.openChange')
-  keymapVscVisual('x', 'za', 'git.stageSelectedRanges')
-  keymapVscVisual('x', 'zr', 'git.unstageSelectedRanges')
+  keymapVsc("n", "#", "workbench.action.findInFiles", "{ 'query': expand('<cword>')}")
+  keymapVscVisual("x", "#", "workbench.action.findInFiles", "{ 'query': expand('<cword>')}")
+  -- comment
+  keymapVsc("n", "gcc", "editor.action.commentLine")
+  keymapVscVisual("x", "gc", "editor.action.commentLine")
 else
-  keymap('n', '<CR>', 'o<ESC>')
-  keymap('n', '<C-h>', ':<C-u>bprev<CR>')
-  keymap('n', '<C-l>', ':<C-u>bnext<CR>')
-  keymap('n', '<Space>s', ':<C-u>w<CR>')
-  keymap('n', '<C-q>', '<C-w>w')
+  keymap("n", "<CR>", "o<ESC>")
+  keymap("n", "<C-h>", ":<C-u>bprev<CR>")
+  keymap("n", "<C-l>", ":<C-u>bnext<CR>")
+  keymap("n", "<Space>s", ":<C-u>w<CR>")
+  keymap("n", "<C-q>", "<C-w>w")
 end
--- dotfiles/dot_config/nvim/lua/utils/setKeymap.lua
+++ dotfiles/dot_config/nvim/lua/utils/setKeymap.lua
@@ -1,9 +1,9 @@
-local function _getModes(modeStr)
+local getModesTableFromStr = function(modeStr)
   -- 文字列を1字ずつ分割し配列に変換
-  -- ex) 'nx' => {'n', 'x'}
+  -- ex) "nx" => {"n", "x"}
   local modes = {}
-  if modeStr == '' then
-    table.insert(modes, '')
+  if modeStr == "" then
+    table.insert(modes, "")
   else
     for i = 1, string.len(modeStr) do
       local mode = string.sub(modeStr, i, i)
@@ -13,42 +13,66 @@ local function _getModes(modeStr)
   return modes
 end
 
-local function _setKeymap(modeStr, lhs, rhs, opts)
-  local modes = _getModes(modeStr)
+local appendCommonOpts = function(opts)
   local _opts = opts or {}
-  _opts.noremap = true
-  _opts.silent = true
-  vim.keymap.set(modes, lhs, rhs, _opts)
+
+  if _opts.noremap == nil then
+    _opts.noremap = true
+  end
+  if _opts.silent == nil then
+    _opts.silent = true
+  end
+
+  return _opts
+end
+
+local getRhsFromVsCmd = function(cmd, vs_args)
+  local rhs
+  if vs_args ~= nil then
+    rhs = string.format("<Cmd> call VSCodeNotify('%s', %s)<Cr>", cmd, vs_args)
+  else
+    rhs = string.format("<Cmd> call VSCodeNotify('%s')<Cr>", cmd)
+  end
+
+  return rhs
+end
+
+local getRhsFromVsVisualCmd = function(cmd, vs_args)
+  local rhs
+  if vs_args ~= nil then
+    -- <Cmd> call VSCodeNotifyVisual()<Cr> の後に <Esc> を追記することでコマンド実行後にノーマルモードに移行する
+    -- VSCodeNotifyVisual の第二引数に 0 を入れることで､コマンド実行後の vscode の文字選択状態を解除する
+    rhs = string.format("<Cmd> call VSCodeNotifyVisual('%s', 0, %s)<Cr><Esc>", cmd, vs_args)
+  else
+    rhs = string.format("<Cmd> call VSCodeNotifyVisual('%s', 0)<Cr><Esc>", cmd)
+  end
+
+  return rhs
 end
 
 local M = {}
 
 M.keymap = function(modeStr, lhs, rhs, opts)
-  _setKeymap(modeStr, lhs, rhs, opts)
+  local _modes = getModesTableFromStr(modeStr)
+  local _opts = appendCommonOpts(opts)
+
+  vim.keymap.set(_modes, lhs, rhs, _opts)
 end
 
-M.keymapVsc = function(modeStr, lhs, cmd, vs_args)
-  local rhs
-  if vs_args ~= nil then
-    rhs = string.format('<Cmd> call VSCodeNotify("%s", %s)<Cr>', cmd, vs_args)
-  else
-    rhs = string.format('<Cmd> call VSCodeNotify("%s")<Cr>', cmd)
-  end
+M.keymapVsc = function(modeStr, lhs, cmd, vs_args, opts)
+  local _modes = getModesTableFromStr(modeStr)
+  local _rhs = getRhsFromVsCmd(cmd, vs_args)
+  local _opts = appendCommonOpts(opts)
 
-  _setKeymap(modeStr, lhs, rhs)
+  vim.keymap.set(_modes, lhs, _rhs, _opts)
 end
 
-M.keymapVscVisual = function(modeStr, lhs, cmd, vs_args)
-  local rhs
-  if vs_args ~= nil then
-    -- <Cmd> call VSCodeNotifyVisual()<Cr> の後に <Esc> を追記することでコマンド実行後にノーマルモードに移行する
-    -- VSCodeNotifyVisual の第二引数に 0 を入れることで､コマンド実行後の vscode の文字選択状態を解除する
-    rhs = string.format('<Cmd> call VSCodeNotifyVisual("%s", 0, %s)<Cr><Esc>', cmd, vs_args)
-  else
-    rhs = string.format('<Cmd> call VSCodeNotifyVisual("%s", 0)<Cr><Esc>', cmd)
-  end
+M.keymapVscVisual = function(modeStr, lhs, cmd, vs_args, opts)
+  local _modes = getModesTableFromStr(modeStr)
+  local _rhs = getRhsFromVsVisualCmd(cmd, vs_args)
+  local _opts = appendCommonOpts(opts)
 
-  _setKeymap(modeStr, lhs, rhs)
+  vim.keymap.set(_modes, lhs, _rhs, _opts)
 end
 
 return M
