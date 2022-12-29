459a8499ede04c7bcd2a701f183cf1a38ec9c472
r-okm <r.okm.00113@gmail.com>
2022-12-29 12:51:12 +0900

    [vim] workspaces hooks でファイルバッファにフォーカス

--- dotfiles/dot_config/nvim/lua/plugins/appearance/workspaces/config.lua
+++ dotfiles/dot_config/nvim/lua/plugins/appearance/workspaces/config.lua
@@ -1,7 +1,7 @@
 return function()
   require("workspaces").setup {
     hooks = {
-      open = { "Trouble", "NvimTreeOpen" }
+      open = { "Trouble", "NvimTreeOpen", "wincmd w" }
     }
   }
 end
