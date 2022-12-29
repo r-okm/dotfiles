141a9a53a7f57d70a5e6069f88b2a5e04c10751d
r-okm <r.okm.00113@gmail.com>
2022-12-29 10:14:04 +0900

    [vim] semicolon

--- dotfiles/dot_config/nvim/lua/plugins/appearance/init.lua
+++ dotfiles/dot_config/nvim/lua/plugins/appearance/init.lua
@@ -1,86 +1,86 @@
 return function(use)
   -- treesitter
   use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-    event = { 'BufRead', 'BufNewFile' },
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+    event = { "BufRead", "BufNewFile" },
     cond = function()
       return not vim.g.vscode
     end,
-    config = require('plugins.appearance.treesitter.config'),
+    config = require("plugins.appearance.treesitter.config"),
   }
   use {
-    'windwp/nvim-autopairs',
-    event = { 'InsertEnter' },
-    wants = { 'nvim-treesitter', 'nvim-cmp' },
+    "windwp/nvim-autopairs",
+    event = { "InsertEnter" },
+    wants = { "nvim-treesitter", "nvim-cmp" },
     cond = function()
       return not vim.g.vscode
     end,
-    config = require('plugins.appearance.autopairs.config'),
+    config = require("plugins.appearance.autopairs.config"),
   }
   use {
-    'RRethy/vim-illuminate',
-    event = { 'BufRead', 'BufNewFile' },
-    wants = { 'nvim-treesitter' },
+    "RRethy/vim-illuminate",
+    event = { "BufRead", "BufNewFile" },
+    wants = { "nvim-treesitter" },
     cond = function()
       return not vim.g.vscode
     end,
     config = function()
       local set_hl = vim.api.nvim_set_hl
-      local bg_color = '#425259'
+      local bg_color = "#425259"
 
-      set_hl(0, 'IlluminatedWordText', { bg = bg_color })
-      set_hl(0, 'IlluminatedWordRead', { bg = bg_color })
-      set_hl(0, 'IlluminatedWordWrite', { bg = bg_color })
+      set_hl(0, "IlluminatedWordText", { bg = bg_color })
+      set_hl(0, "IlluminatedWordRead", { bg = bg_color })
+      set_hl(0, "IlluminatedWordWrite", { bg = bg_color })
     end,
   }
 
   -- telescope
   use {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.0',
-    module = 'telescope',
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.0",
+    module = "telescope",
     requires = {
-      { 'nvim-lua/plenary.nvim', opt = true, },
-      { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', opt = true, },
-      { 'AckslD/nvim-neoclip.lua', config = require('plugins.appearance.neoclip.config'), opt = true, },
-      { 'natecraddock/workspaces.nvim', config = require('plugins.appearance.workspaces.config'), opt = true, },
+      { "nvim-lua/plenary.nvim", opt = true, },
+      { "nvim-telescope/telescope-fzf-native.nvim", run = "make", opt = true, },
+      { "AckslD/nvim-neoclip.lua", config = require("plugins.appearance.neoclip.config"), opt = true, },
+      { "natecraddock/workspaces.nvim", config = require("plugins.appearance.workspaces.config"), opt = true, },
     },
     wants = {
-      'plenary.nvim',
-      'telescope-fzf-native.nvim',
-      'nvim-neoclip.lua',
-      'workspaces.nvim',
+      "plenary.nvim",
+      "telescope-fzf-native.nvim",
+      "nvim-neoclip.lua",
+      "workspaces.nvim",
     },
     cond = function()
       return not vim.g.vscode
     end,
-    setup = require('plugins.appearance.telescope.setup'),
-    config = require('plugins.appearance.telescope.config'),
+    setup = require("plugins.appearance.telescope.setup"),
+    config = require("plugins.appearance.telescope.config"),
   }
   use {
-    'aznhe21/actions-preview.nvim',
-    event = { 'BufRead', 'BufNewFile', },
+    "aznhe21/actions-preview.nvim",
+    event = { "BufRead", "BufNewFile", },
     cond = function()
       return not vim.g.vscode
     end,
     config = function()
-      require('actions-preview').setup {
+      require("actions-preview").setup {
         telescope = {
-          initial_mode = 'normal',
+          initial_mode = "normal",
           defaults = {
             mappings = {
               n = {
-                ['q'] = require('telescope.actions').close
+                ["q"] = require("telescope.actions").close
               }
             }
           },
-          sorting_strategy = 'ascending',
-          layout_strategy = 'vertical',
+          sorting_strategy = "ascending",
+          layout_strategy = "vertical",
           layout_config = {
             width = 0.8,
             height = 0.9,
-            prompt_position = 'top',
+            prompt_position = "top",
             preview_cutoff = 20,
             preview_height = function(_, _, max_lines)
               return max_lines - 15
@@ -111,43 +111,43 @@ return function(use)
 
   -- file tree
   use {
-    'nvim-tree/nvim-tree.lua',
+    "nvim-tree/nvim-tree.lua",
     requires = {
-      { 'nvim-tree/nvim-web-devicons', opt = true },
+      { "nvim-tree/nvim-web-devicons", opt = true },
     },
-    wants = { 'nvim-web-devicons' },
+    wants = { "nvim-web-devicons" },
     cmd = { "NvimTreeOpen", "NvimTreeFindFile" },
     cond = function()
       return not vim.g.vscode
     end,
-    config = require('plugins.appearance.nvim-tree.config'),
+    config = require("plugins.appearance.nvim-tree.config"),
   }
 
   -- Comment
   use {
-    'numToStr/Comment.nvim',
-    event = { 'BufRead', 'BufNewFile', },
+    "numToStr/Comment.nvim",
+    event = { "BufRead", "BufNewFile", },
     cond = function()
       return not vim.g.vscode
     end,
     config = function()
-      require('Comment').setup()
+      require("Comment").setup()
     end,
   }
   -- indent line
   use {
-    'lukas-reineke/indent-blankline.nvim',
-    event = { 'BufRead', 'BufNewFile' },
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufRead", "BufNewFile" },
     cond = function()
       return not vim.g.vscode
     end,
-    config = require('plugins.appearance.indent-blankline.config'),
+    config = require("plugins.appearance.indent-blankline.config"),
   }
 
   -- color
   use {
-    'norcalli/nvim-colorizer.lua',
-    event = { 'BufRead', 'BufNewFile' },
+    "norcalli/nvim-colorizer.lua",
+    event = { "BufRead", "BufNewFile" },
     cond = function()
       return not vim.g.vscode
     end,
@@ -155,8 +155,8 @@ return function(use)
       local css_opts = {
         rgb_fn = true, names = true
       }
-      require('colorizer').setup({
-        '*';
+      require("colorizer").setup({
+        "*";
         css = css_opts,
         scss = css_opts,
         stylus = css_opts,
@@ -166,67 +166,67 @@ return function(use)
 
   -- terminal
   use {
-    'akinsho/toggleterm.nvim',
+    "akinsho/toggleterm.nvim",
     cond = function()
       return not vim.g.vscode
     end,
-    config = require('plugins.appearance.toggleterm.config'),
+    config = require("plugins.appearance.toggleterm.config"),
   }
 
   -- tab
   use {
-    'akinsho/bufferline.nvim',
-    event = { 'BufRead', 'BufNewFile' },
+    "akinsho/bufferline.nvim",
+    event = { "BufRead", "BufNewFile" },
     requires = {
-      { 'nvim-tree/nvim-web-devicons', opt = true },
+      { "nvim-tree/nvim-web-devicons", opt = true },
     },
-    wants = { 'nvim-web-devicons' },
+    wants = { "nvim-web-devicons" },
     cond = function()
       return not vim.g.vscode
     end,
-    config = require('plugins.appearance.bufferline.config'),
+    config = require("plugins.appearance.bufferline.config"),
   }
 
   -- delete buffer without closing window
   use {
-    'famiu/bufdelete.nvim',
+    "famiu/bufdelete.nvim",
     cmd = {
-      'Bdelete',
-      'Bwipeout',
+      "Bdelete",
+      "Bwipeout",
     },
     setup = function()
       if vim.g.vscode then
         return
       end
 
-      local keymap = require('utils.setKeymap').keymap
-      keymap('n', '<Space>w', ':<C-u>Bdelete<CR>')
+      local keymap = require("utils.setKeymap").keymap
+      keymap("n", "<Space>w", ":<C-u>Bdelete<CR>")
     end,
   }
 
   -- statusbar
   use {
-    'nvim-lualine/lualine.nvim',
+    "nvim-lualine/lualine.nvim",
     requires = {
-      { 'kyazdani42/nvim-web-devicons', as = 'lualine-web-devicons', opt = true }
+      { "kyazdani42/nvim-web-devicons", as = "lualine-web-devicons", opt = true }
     },
     cond = function()
       return not vim.g.vscode
     end,
     config = function()
-      require('lualine').setup()
+      require("lualine").setup()
     end,
   }
 
   -- gitsign
   use {
-    'lewis6991/gitsigns.nvim',
-    event = { 'BufRead', 'BufNewFile' },
+    "lewis6991/gitsigns.nvim",
+    event = { "BufRead", "BufNewFile" },
     cond = function()
       return not vim.g.vscode
     end,
     config = function()
-      require('gitsigns').setup()
+      require("gitsigns").setup()
     end,
   }
 
@@ -248,17 +248,17 @@ return function(use)
     end,
   }
   -- use {
-  --   'rebelot/kanagawa.nvim',
+  --   "rebelot/kanagawa.nvim",
   --   cond = function()
   --     return not vim.g.vscode
   --   end,
   --   config = function()
-  --     require('kanagawa').setup({
+  --     require("kanagawa").setup({
   --       commentStyle = { italic = false },
   --       keywordStyle = { italic = false },
   --       variablebuiltinStyle = { italic = false },
   --       overrides = {
-  --         Visual = { bg = '#59594e' },
+  --         Visual = { bg = "#59594e" },
   --       },
   --     })
   --     vim.cmd("colorscheme kanagawa")
