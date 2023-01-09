fc704f33141015ae174b348edfa92c481341b7c1
r-okm <r.okm.00113@gmail.com>
2023-01-09 17:13:00 +0900

    [vim] tailwindcss lsp の root_dir を taiwind.config ファイルのみに変更

--- dotfiles/dot_config/nvim/lua/lsp/servers/tailwindcss.lua
+++ dotfiles/dot_config/nvim/lua/lsp/servers/tailwindcss.lua
@@ -1,5 +1,6 @@
 local M = {
   capabilities = require("cmp_nvim_lsp").default_capabilities(),
+  root_dir = require("lspconfig.util").root_pattern("tailwind.config.js", "tailwind.config.ts")
 }
 
 return M
