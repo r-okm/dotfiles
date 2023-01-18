36f990b71e3428bcf4a30faae79520c47bbf5a1c
r-okm <r.okm.00113@gmail.com>
2023-01-18 18:56:28 +0900

    chezmoi.toml の内容を templates ディレクトリに移動、chezmoi バイナリのインストール場所変更

--- install.sh
+++ install.sh
@@ -5,17 +5,8 @@ cd `dirname $0`
 chezmoi_data_file="$HOME/.config/chezmoi/chezmoi.toml"
 if [ ! -f $chezmoi_data_file ]; then
   mkdir -p $(dirname $chezmoi_data_file)
-  cat << EOF > $chezmoi_data_file
-[merge]
-  command = "nvim"
-  args = ["-d", "{{ .Destination }}", "{{ .Source }}"]
-[data]
-  name_home = ""
-  email_home = ""
-  name_work = ""
-  email_work = ""
-  openai_api_key = ""
-EOF
+  cat ./templates/chezmoi.toml > $chezmoi_data_file
 fi
 
-sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply -S .
+sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
+$HOME/.local/bin/chezmoi init --apply -S .
