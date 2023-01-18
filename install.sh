236ba0854180bb3ae5285c0a56ef827becb9584d
r-okm <r.okm.00113@gmail.com>
2023-01-18 12:53:43 +0900

    .chezmoiscripts を install.sh に集約

--- install.sh
+++ install.sh
@@ -2,4 +2,18 @@
 
 cd `dirname $0`
 
+chezmoi_data_file="$HOME/.config/chezmoi/chezmoi.toml"
+[ -f $chezmoi_data_file ] || mkdir -p $(dirname $chezmoi_data_file) && touch $chezmoi_data_file
+cat << EOF > $chezmoi_data_file
+[merge]
+  command = "nvim"
+  args = ["-d", "{{ .Destination }}", "{{ .Source }}"]
+[data]
+  name_home = ""
+  email_home = ""
+  name_work = ""
+  email_work = ""
+  openai_api_key = ""
+EOF
+
 sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply -S .
