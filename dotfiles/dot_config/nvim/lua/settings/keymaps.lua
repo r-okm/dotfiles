6961d7540ad16d26f50c40620e03c7de19f5bc43
r-okm <r.okm.00113@gmail.com>
2023-04-26 09:43:46 +0900

    [vim] space を用いた buffer のショートカットを削除

--- dotfiles/dot_config/nvim/lua/settings/keymaps.lua
+++ dotfiles/dot_config/nvim/lua/settings/keymaps.lua
@@ -14,11 +14,6 @@ keymap("n", "<CR>", "o<ESC>")
 
 -- vscode
 if vim.g.vscode then
-  -- buffer
-  keymapVsc("n", "<Space>s", "workbench.action.files.save")
-  keymapVsc("n", "<Space>S", "workbench.action.files.saveWithoutFormatting")
-  keymapVsc("n", "<Space>w", "workbench.action.closeActiveEditor")
-  keymapVsc("n", "<Space>t", "workbench.action.reopenClosedEditor")
   -- filer
   keymapVsc("n", "<Space>c", "workbench.action.closeSidebar")
   keymapVsc("n", "<Space>e", "workbench.view.explorer")
@@ -45,7 +40,5 @@ else
   keymap("nx", "k", "gk")
   keymap("n", "<C-h>", ":<C-u>bprev<CR>")
   keymap("n", "<C-l>", ":<C-u>bnext<CR>")
-  keymap("n", "<Space>s", ":<C-u>w<CR>")
-  keymap("n", "<Space>S", ":<C-u>noa w<CR>")
   keymap("n", "<C-q>", "<C-w>w")
 end
