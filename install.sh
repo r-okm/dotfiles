5a69d8f067fe633218507e85b7f48b64bba0d377
r-okm <r.okm.00113@gmail.com>
2023-11-07 17:35:05 +0900

    [install] update

--- install.sh
+++ install.sh
@@ -13,3 +13,12 @@ $chezmoi_bin init $github_username --apply
 
 # install linuxbrew
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# restore brew bundle
+export HOMEBREW_BUNDLE_FILE="$HOME/.config/homebrew/.Brewfile"
+/home/linuxbrew/.linuxbrew/bin/brew bundle
+
+# change login shell
+zsh_bin='/home/linuxbrew/.linuxbrew/bin/zsh'
+echo $zsh_bin | sudo tee -a /etc/shells
+chsh -s $(echo $zsh_bin)
