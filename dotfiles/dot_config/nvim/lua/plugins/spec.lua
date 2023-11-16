04ad3653048c15520b73c787afff8e45086acdc1
r-okm <r.okm.00113@gmail.com>
2023-11-16 14:34:51 +0900

    [nvim] modify bufferline style

--- dotfiles/dot_config/nvim/lua/plugins/spec.lua
+++ dotfiles/dot_config/nvim/lua/plugins/spec.lua
@@ -348,14 +348,13 @@ return {
       local bufferline = require("bufferline")
       bufferline.setup({
         options = {
-          style_preset = bufferline.style_preset.no_italic,
           middle_mouse_command = "Bdelete %d",
           diagnostics = "coc",
-          separator_style = { "╱", "╱" },
-          show_buffer_close_icons = true,
+          separator_style = { "│", "│" },
+          show_buffer_close_icons = false,
           indicator = {
-            -- icon = "",
-            style = "underline",
+            icon = "",
+            style = "icon",
           },
         },
       })
