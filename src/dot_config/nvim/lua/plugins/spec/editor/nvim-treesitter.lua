25f6070eda2b6e93ef5f1226436fa3cd3a0e3995
r-okm <r.okm.00113@gmail.com>
2023-12-05 14:45:53 +0900

    [nvim] disable highlight for large file

--- src/dot_config/nvim/lua/plugins/spec/editor/nvim-treesitter.lua
+++ src/dot_config/nvim/lua/plugins/spec/editor/nvim-treesitter.lua
@@ -1,16 +1,21 @@
+local const = require("utils.const")
+
 return {
   "nvim-treesitter/nvim-treesitter",
   event = { "BufReadPost" },
   build = { ":TSUpdate" },
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "bash", "c", "c_sharp", "cpp", "css", "diff", "dockerfile", "git_config",
-        "git_rebase", "gitattributes", "gitcommit", "gitignore", "html", "java", "javascript", "jq", "jsdoc", "json",
-        "jsonc", "lua", "markdown", "markdown_inline", "sql", "terraform", "toml", "tsx",
+      ensure_installed = { "bash", "css", "diff", "dockerfile", "git_config",
+        "git_rebase", "gitattributes", "gitcommit", "gitignore", "html", "javascript", "jq", "jsdoc", "json",
+        "jsonc", "lua", "markdown", "markdown_inline", "terraform", "toml", "tsx",
         "typescript", "vim", "vimdoc",
       },
       highlight = {
         enable = true,
+        disable = function(_, bufnr)
+          return vim.api.nvim_buf_line_count(bufnr) > const.LARGE_FILE_LINE_COUNT
+        end,
       },
     })
   end,
