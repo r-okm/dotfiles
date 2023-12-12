88d47f196d24a3653dc4a05e5112ac080539c3c7
r-okm <r.okm.00113@gmail.com>
2023-12-12 17:11:19 +0900

    [nvim] change coc hover function

--- src/dot_config/nvim/lua/plugins/spec/cmp/coc.lua
+++ src/dot_config/nvim/lua/plugins/spec/cmp/coc.lua
@@ -25,7 +25,7 @@ return {
       if vim.fn.index({ "vim", "help" }, vim.bo.filetype) >= 0 then
         vim.api.nvim_command("h " .. cw)
       elseif vim.api.nvim_eval("coc#rpc#ready()") then
-        vim.fn.CocActionAsync("doHover")
+        vim.fn.CocActionAsync("definitionHover")
       else
         vim.api.nvim_command("!" .. vim.o.keywordprg .. " " .. cw)
       end
