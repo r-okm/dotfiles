33e2a00bbfeda3ab4e913e074f820ab81e8ab5fb
r-okm <r.okm.00113@gmail.com>
2023-12-12 15:53:54 +0900

    [nvim] add 'man' to HIGHLIGHT_DISABLED_FILETYPES

--- src/dot_config/nvim/lua/utils/const.lua
+++ src/dot_config/nvim/lua/utils/const.lua
@@ -3,6 +3,6 @@ local M = {}
 -- 巨大ファイルと判定する行数
 M.LARGE_FILE_LINE_COUNT = 5000
 -- ハイライトを無効にするファイルタイプのリスト
-M.HIGHLIGHT_DISABLED_FILETYPES = { "help", "lazy", "fern" }
+M.HIGHLIGHT_DISABLED_FILETYPES = { "help", "lazy", "fern", "man" }
 
 return M
