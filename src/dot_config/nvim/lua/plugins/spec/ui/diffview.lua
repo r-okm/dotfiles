1872e8c14081795c78ab94348408479e2d75718f
r-okm <r.okm.00113@gmail.com>
2023-12-22 13:45:12 +0900

    [nvim] diffview, add keymaps and disable indent-blankline

--- src/dot_config/nvim/lua/plugins/spec/ui/diffview.lua
+++ src/dot_config/nvim/lua/plugins/spec/ui/diffview.lua
@@ -10,6 +10,25 @@ return {
   },
   cmd = { "DiffviewFileHistory" },
   config = function()
+    -- diffview では indent-blankline を無効にする
+    vim.api.nvim_create_autocmd("BufEnter", {
+      callback = function()
+        local diffview = require("diffview.lib").get_current_view()
+        if diffview then
+          require("ibl").setup_buffer(0, { enabled = false })
+        end
+      end,
+    })
     keymap("n", "zh", ":<C-u>DiffviewFileHistory %<CR>")
+    require("diffview").setup({
+      keymaps = {
+        view = {
+          { "n", "q", ":<C-u>tabc<CR>" },
+        },
+        file_history_panel = {
+          { "n", "q", ":<C-u>tabc<CR>" },
+        },
+      },
+    })
   end,
 }
