cf0a6f6a1e2b8d61e7cf554f446c68d988f62b34
r-okm <r.okm.00113@gmail.com>
2023-12-26 19:02:03 +0900

    [nvim] add toggleterm keymap for git log

--- src/dot_config/nvim/lua/plugins/spec/ui/toggleterm.lua
+++ src/dot_config/nvim/lua/plugins/spec/ui/toggleterm.lua
@@ -4,6 +4,7 @@ return {
   "akinsho/toggleterm.nvim",
   keys = {
     { "zg", mode = { "n" } },
+    { "zl", mode = { "n" } },
   },
   config = function()
     local Terminal = require("toggleterm.terminal").Terminal
@@ -12,11 +13,20 @@ return {
       direction = "float",
       hidden = true,
     })
+    local gitLog = Terminal:new({
+      cmd = "git lga",
+      direction = "float",
+      hidden = true,
+    })
 
     function LazygitToggle()
       lazygit:toggle()
     end
+    function GitLogToggle()
+      gitLog:toggle()
+    end
 
     keymap("n", "zg", "<cmd>lua LazygitToggle()<CR>")
+    keymap("n", "zl", "<cmd>lua GitLogToggle()<CR>")
   end,
 }
