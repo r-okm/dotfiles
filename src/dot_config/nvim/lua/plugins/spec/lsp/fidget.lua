91fb828bd2e419caa5994e7b10f47fe0ae996c1d
r-okm <r.okm.00113@gmail.com>
2024-02-28 18:04:05 +0900

    [nvim] ignore null-ls in fidget

--- src/dot_config/nvim/lua/plugins/spec/lsp/fidget.lua
+++ src/dot_config/nvim/lua/plugins/spec/lsp/fidget.lua
@@ -1,6 +1,10 @@
 return {
   "j-hui/fidget.nvim",
   init = function()
-    require("fidget").setup()
+    require("fidget").setup({
+      progress = {
+        ignore = { "null-ls" },
+      },
+    })
   end,
 }
