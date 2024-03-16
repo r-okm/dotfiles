ae80eca9fdd3de6c33ab51d7897fe62eab92015f
r-okm <r.okm.00113@gmail.com>
2024-03-16 17:04:59 +0900

    [nvim] remove indent-rainbowline

--- src/dot_config/nvim/lua/plugins/spec/terminal/indent-blankline.lua
+++ src/dot_config/nvim/lua/plugins/spec/terminal/indent-blankline.lua
@@ -2,9 +2,6 @@ local const = require("utils.const")
 
 return {
   "lukas-reineke/indent-blankline.nvim",
-  dependencies = {
-    "TheGLander/indent-rainbowline.nvim",
-  },
   main = "ibl",
   event = { "BufReadPost", "BufNewFile" },
   init = function()
@@ -15,21 +12,15 @@ return {
       require("ibl").setup_buffer(0, { enabled = true })
     end
   end,
-  opts = function()
-    local ibl_opts = {
-      indent = {
-        char = "▏",
-      },
-      scope = {
-        enabled = false,
-      },
-      exclude = {
-        filetypes = const.HIGHLIGHT_DISABLED_FILETYPES,
-      },
-    }
-    local irl_opts = {
-      color_transparency = 0.06,
-    }
-    return require("indent-rainbowline").make_opts(ibl_opts, irl_opts)
-  end,
+  opts = {
+    indent = {
+      char = "▏",
+    },
+    scope = {
+      enabled = false,
+    },
+    exclude = {
+      filetypes = const.HIGHLIGHT_DISABLED_FILETYPES,
+    },
+  },
 }
