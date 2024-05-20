0e903ad8d2acf768cf9623736843d91d811aa3b1
r-okm <r.okm.00113@gmail.com>
2024-05-20 17:16:24 +0900

    [nvim] update dependencies

--- src/dot_config/nvim/lua/plugins/spec/terminal/nvim-cmp.lua
+++ src/dot_config/nvim/lua/plugins/spec/terminal/nvim-cmp.lua
@@ -1,5 +1,6 @@
 return {
   "hrsh7th/nvim-cmp",
+  branch = "main",
   dependencies = {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-cmdline",
--- src/dot_config/nvim/lua/plugins/spec/terminal/nvim-ts-autotag.lua
+++ src/dot_config/nvim/lua/plugins/spec/terminal/nvim-ts-autotag.lua
@@ -1,15 +1,13 @@
 return {
   "windwp/nvim-ts-autotag",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-  },
   event = { "BufReadPost", "BufNewFile" },
   config = function()
     require("nvim-ts-autotag").setup({
-      enable = true,
-      enable_rename = true,
-      enable_close = true,
-      enable_close_on_slash = true,
+      opts = {
+        enable_rename = true,
+        enable_close = true,
+        enable_close_on_slash = true,
+      },
     })
   end,
 }
