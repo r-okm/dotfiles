a05bcbb467a450b222c8e87a0ae97739c60ee205
r-okm <r.okm.00113@gmail.com>
2024-06-11 17:38:03 +0900

    fix(nvim): 例外処理を追加して CopilotChat のロードを安全にする
    以前のコードでは、CopilotChat モジュールが存在しない場合、特に CopilotChat を無効にした環境でエラーが発生していた。
    この変更により、CopilotChat モジュールのロード時に例外処理を追加し、存在しない場合でもエラーを防ぐことができるようになった。

--- src/dot_config/nvim/ftplugin/gitcommit.lua
+++ src/dot_config/nvim/ftplugin/gitcommit.lua
@@ -1,9 +1,11 @@
-vim.schedule(function()
-  require("CopilotChat")
-  vim.cmd.CopilotChatCommitStaged()
-end)
-vim.api.nvim_create_autocmd("QuitPre", {
-  command = "CopilotChatClose",
-})
+local ok, _ = pcall(require, "CopilotChat")
+if ok then
+  vim.schedule(function()
+    vim.cmd.CopilotChatCommitStaged()
+  end)
+  vim.api.nvim_create_autocmd("QuitPre", {
+    command = "CopilotChatClose",
+  })
 
-vim.keymap.set("ca", "qq", "execute 'CopilotChatClose' <bar> wqa")
+  vim.keymap.set("ca", "qq", "execute 'CopilotChatClose' <bar> wqa")
+end
