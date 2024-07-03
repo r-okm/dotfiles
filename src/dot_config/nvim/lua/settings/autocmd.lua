ce30fd28b24291c9073c2ded8ccea4e53de23a33
r-okm <r.okm.00113@gmail.com>
2024-07-03 15:17:30 +0900

    fix(nvim): BufWritePre イベントハンドラが別バッファで動作する事象の修正
    - `vim.api.nvim_create_autocmd` 関数の引数にバッファ番号を追加した
      - これにより、各バッファで独立した `BufWritePre` イベントハンドラが動作するようになった

--- src/dot_config/nvim/lua/settings/autocmd.lua
+++ src/dot_config/nvim/lua/settings/autocmd.lua
@@ -79,6 +79,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     if format_on_save then
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = vim.api.nvim_create_augroup("PreWrite" .. client.name .. bufnr, {}),
+        buffer = bufnr,
         callback = function()
           vim.lsp.buf.format({
             async = false,
