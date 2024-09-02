9ed210bc7550018ad86bb96d8700cab99ce8d538
r-okm <r.okm.00113@gmail.com>
2024-09-02 16:25:55 +0900

    feat(devcontainer): devcontainer 用 install.sh 追加

---
+++ install_devcontainer.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+# install chezmoi then apply dotfiles
+BIN_DIR="$HOME/.local/bin"
+mkdir -p "$BIN_DIR"
+PATH="$BIN_DIR:$PATH"
+
+sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$BIN_DIR"
+chezmoi --apply
+
+# install linuxbrew
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install tools with brew
+/home/linuxbrew/.linuxbrew/bin/brew install \
+  bat \
+  fd \
+  fzf \
+  git-delta \
+  lazygit \
+  ripgrep \
+  zsh
