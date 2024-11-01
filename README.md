878d4ef3cf0a3b00d2119dd3976acff3acea8c9e
r-okm <r.okm.00113@gmail.com>
2024-11-01 13:05:03 +0900

    chore: update README and install scripts
    - Updated README.md to include new installation instructions for dotfiles
      and nvim-config.
    - Modified install.sh to include a function for installing nvim-config.
    - Removed install_devcontainer.sh as it is no longer needed.

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # dotfiles
 
-## Install
+## Apply dotfiles
+
+- dotfiles only
 
 ```sh
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/r-okm/dotfiles/main/install.sh)"
+```
+
+- dotfiles and nvim-config
+
+```sh
+INSTALL_NVIM_CONFIG=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/r-okm/dotfiles/main/install.sh)"
 ```
 
 ## Change login shell
@@ -11,19 +19,52 @@
 ```sh
 zsh_bin='/home/linuxbrew/.linuxbrew/bin/zsh'
 echo $zsh_bin | sudo tee -a /etc/shells
-chsh -s $(echo $zsh_bin)
+chsh -s $zsh_bin
 ```
 
-## Insert value to chezmoi data file
+## Update machine-specific settings using chezmoi data
 
-edit data file
+1. edit chezmoi data file
+
+   ```sh
+   vi ~/.config/chezmoi/chezmoi.toml
+   ```
+
+1. apply edited data
+
+   ```sh
+   chezmoi apply
+   ```
+
+## Install linuxbrew and packages
 
 ```sh
-$EDITOR ~/.config/chezmoi/chezmoi.toml
+# install linuxbrew
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# restore brew bundle
+export HOMEBREW_BUNDLE_FILE="$HOME/.config/homebrew/.Brewfile"
+/home/linuxbrew/.linuxbrew/bin/brew bundle
 ```
 
-apply edited data
+## Install docker
 
 ```sh
-chezmoi apply
+sudo apt-get update
+sudo apt-get install -y \
+  ca-certificates \
+  gnupg \
+  lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+sudo apt-get update
+sudo apt-get install -y \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io \
+  docker-compose-plugin
+
+sudo usermod -aG docker "$USER"
 ```
--- install.sh
+++ install.sh
@@ -1,55 +1,22 @@
 #!/usr/bin/env bash
-
 set -euxo pipefail
 
-# install prerequisites
-sudo apt-get update
-sudo apt-get install -y \
-  build-essential \
-  procps \
-  curl \
-  file \
-  git \
-  unzip
+install_nvim_config() {
+  local NVIM_CONFIG_DIR="$HOME/.config/nvim"
+  local REPO_URL='https://github.com/r-okm/nvim-config.git'
 
-# install chezmoi then apply dotfiles
-BIN_DIR="$HOME/.local/bin"
-mkdir -p "$BIN_DIR"
-PATH="$BIN_DIR:$PATH"
-GITHUB_USERNAME='r-okm'
+  git clone "$REPO_URL" "$NVIM_CONFIG_DIR"
+}
 
-sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$BIN_DIR"
-chezmoi init $GITHUB_USERNAME --apply
+main() {
+  BIN_DIR="$HOME/.local/bin"
+  mkdir -p "$BIN_DIR"
+  PATH="$BIN_DIR:$PATH"
 
-# install linuxbrew
-NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$BIN_DIR"
+  chezmoi apply
 
-# restore brew bundle
-export HOMEBREW_BUNDLE_FILE="$HOME/.config/homebrew/.Brewfile"
-/home/linuxbrew/.linuxbrew/bin/brew bundle
+  [[ $INSTALL_NVIM_CONFIG -eq 1 ]] && install_nvim_config
+}
 
-# change locale
-LOCALE_JA='/usr/share/i18n/locales/ja_JP'
-if [ -e "$LOCALE_JA" ]; then
-  sudo localedef -i $LOCALE_JA -f UTF-8 /usr/lib/locale/ja_JP.UTF-8
-  sudo localedef --add-to-archive /usr/lib/locale/ja_JP.UTF-8
-  sudo localectl set-locale LANG=ja_JP.UTF-8
-fi
-
-# install docker
-sudo apt-get install -y \
-  ca-certificates \
-  gnupg \
-  lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
-sudo apt-get update
-sudo apt-get install -y \
-  docker-ce \
-  docker-ce-cli \
-  containerd.io \
-  docker-compose-plugin
-
-sudo usermod -aG docker "$USER"
+main "$@"
