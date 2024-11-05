19618ab7df76ed0d0f82eb354008d8abc368a369
r-okm <r.okm.00113@gmail.com>
2024-11-05 11:21:18 +0900

    fix(install.sh): use variable for chezmoi executable path

--- install.sh
+++ install.sh
@@ -11,19 +11,20 @@ install_nvim_config() {
 main() {
   # Install chezmoi executable
   BIN_DIR="$HOME/.local/bin"
-  mkdir -p "$BIN_DIR"
-  PATH="$BIN_DIR:$PATH"
+  [[ ! -d "$BIN_DIR" ]] && mkdir -p "$BIN_DIR"
   sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$BIN_DIR"
+  CHEZMOI_EXECUTABLE="$BIN_DIR/chezmoi"
 
   # Install dotfiles using chezmoi
   SOURCE_DIR="${XDG_SHARE_HOME:-$HOME/.local/share}/chezmoi"
   if [[ -d "$SOURCE_DIR" ]]; then
-    chezmoi apply
+    $CHEZMOI_EXECUTABLE apply
   else
     GITHUB_USERNAME='r-okm'
-    chezmoi init $GITHUB_USERNAME --apply
+    $CHEZMOI_EXECUTABLE init $GITHUB_USERNAME --apply
   fi
 
+  # Install neovim config if needed
   [[ $INSTALL_NVIM_CONFIG -eq 1 ]] && install_nvim_config
 }
 
