ac6b1c048e23f20857b962804fa5d570511d4b86
r-okm <65703649+r-okm@users.noreply.github.com>
2025-01-29 16:49:48 +0900

    refactor: move `fzf-git-branch` and `fzf-git-log` from `functions.zsh` to bin dir

--- src/private_dot_config/zsh/defer/functions.zsh
+++ src/private_dot_config/zsh/defer/functions.zsh
@@ -37,80 +37,6 @@ fzf_cd_ghq() {
   fi
 }
 
-fzf_fd_file() {
-  local target_dir="."
-  if [ -n "$1" ]; then
-    target_dir="$1"
-  fi
-
-  local target_file=$(
-    fd --type file \
-      --hidden \
-      --exclude .git \
-      $target_dir \
-    | fzf \
-    --reverse \
-    --prompt='GREP FILE > ' \
-    --header='ENTER to edit' \
-    --preview="bat --color=always --style=auto {}"
-  )
-
-  if [ -n "$target_file" ]; then
-    $EDITOR "$target_file"
-  fi
-}
-
-fzf_git_branch() {
-  local_brs=$(git branch | sort -r | sed -e 's/^ *//') && \
-  local_brs_trim=$(git branch | sed -e 's/\*//' | sed -e 's/ //g') && \
-  remote_brs=$(git branch -r | grep -v HEAD | sed -e 's/^.* //g') && \
-
-  remote_brs_trim=$(echo $remote_brs | sed -e 's/^.*\///')
-  local_remote_brs=$(echo $local_brs_trim\\n$remote_brs_trim | sort | uniq -D)
-  remote_only_brs=$(echo $local_remote_brs\\n$remote_brs_trim | sort | uniq -u)
-  target_brs=$(echo $local_brs\\n$remote_brs\\n$remote_only_brs)
-
-  get_branch_name="echo {} | tr -d ' *'"
-  checkout_branch="$get_branch_name | xargs git checkout"
-  delete_branch="$get_branch_name | xargs git branch -D"
-  view_diff="$get_branch_name | xargs git diff"
-
-  echo $target_brs |
-  fzf --ansi --no-sort --reverse --tiebreak=index \
-    --height 50% \
-    --prompt='CHECKOUT BRANCH > '  \
-    --header='ENTER to checkout, CTRL-d to view diff, CTRL-x to delete' \
-    --preview-window='right,65%' \
-    --preview="$get_branch_name | xargs -I % sh -c 'git fzf-log %'" \
-    --bind "enter:become:$checkout_branch" \
-    --bind "ctrl-x:become:$delete_branch" \
-    --bind "ctrl-d:execute:$view_diff" \
-}
-
-fzf_git_log() {
-  local command="git fzf-log"
-  if [ "$1" = "--all" ]; then
-    command="git fzf-log $1"
-  fi
-
-  local log_line_to_hash="echo {} | grep -o '[a-f0-9]\{7\}' | head -1 | xargs -I % sh -c 'echo -n %'"
-  local preview_commit="$log_line_to_hash | xargs -I % sh -c 'git show -m % | delta --features=fzf_git_log'"
-  local show_commit="$log_line_to_hash | xargs -I % sh -c 'git show -m % | delta --features=git --paging=always'"
-  local checkout_commit="$log_line_to_hash | xargs -I % sh -c 'git switch -d %'"
-  local copy_commit_hash="$log_line_to_hash | xclip -selection clipboard"
-
-  local result=$(
-    eval "$command" | \
-      fzf --ansi --no-sort --reverse --tiebreak=index \
-        --preview=$preview_commit \
-        --header='ENTER to view, CTRL-x to checkout, CTRL-y to copy hash, q to exit' \
-        --bind='j:down,k:up,d:half-page-down,u:half-page-up,q:abort' \
-        --bind "enter:execute:$show_commit" \
-        --bind "ctrl-x:become:$checkout_commit" \
-        --bind "ctrl-y:execute-silent:$copy_commit_hash"
-  )
-}
-
 awsp() {
   local profile=$(
     aws configure list-profiles |
@@ -126,14 +52,6 @@ awsp() {
     unset AWS_PROFILE
     echo "unset AWS_PROFILE"
   fi
-}
-
-# local-ssl-proxy
-local_ssl_proxy() {
-  local source=${1:-8081}
-  local target=${2:-8080}
-
-  npx local-ssl-proxy --key ~/pem/localhost-key.pem --cert ~/pem/localhost.pem --source $source --target $target
 }
 
 update_plugins() {
