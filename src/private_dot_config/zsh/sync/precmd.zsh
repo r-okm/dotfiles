2caf3e58ddb7feec39962ca3d5e33de16fdd00e6
r-okm <65703649+r-okm@users.noreply.github.com>
2025-02-08 17:58:43 +0900

    zsh: precmd hook

---
+++ src/private_dot_config/zsh/sync/precmd.zsh
@@ -0,0 +1,22 @@
+windows_terminal_osc_9_9() {
+  # Inform Terminal about shell current working directory
+  # see: https://github.com/microsoft/terminal/issues/8166
+  printf '\e]9;9;%s\e\' "$(wslpath -w "$(pwd)")"
+}
+
+windows_terminal_tab_title() {
+  local current_dir=$(basename $PWD)
+  echo -ne '\033]0;'"[$WSL_DISTRO_NAME] $current_dir"'\a'
+}
+
+if [[ -n "$WSL_DISTRO_NAME" ]] && [[ -x "$(command -v wslpath)" ]]; then
+  autoload -Uz add-zsh-hook
+  add-zsh-hook precmd windows_terminal_osc_9_9
+  add-zsh-hook precmd windows_terminal_tab_title
+fi
+
+SUCCESS=$'\n%F{yellow}%~%f [%F{reset}%?%f] %# '
+FAILURE=$'\n%F{yellow}%~%f [%F{red}%?%f] %# '
+PROMPT=%(?.$SUCCESS.$FAILURE)
+
+unset SUCCESS FAILURE
