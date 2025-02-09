2d0015ab2a6374269545d17b774e53de2589d042
r-okm <65703649+r-okm@users.noreply.github.com>
2025-02-09 10:36:26 +0900

    rust: install without modifing path

--- src/.chezmoiscripts/run_once_101-install-rust.sh
+++ src/.chezmoiscripts/run_once_101-install-rust.sh
@@ -2,7 +2,8 @@
 set -euxo pipefail
 
 main() {
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs \
+    | sh -s -- -y --no-update-default-toolchain --no-modify-path
 }
 
 main
