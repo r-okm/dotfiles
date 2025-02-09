a0e79d8d33cfc57ac0af8b7efb116254baa9a09a
r-okm <65703649+r-okm@users.noreply.github.com>
2025-02-09 18:08:02 +0900

    asdf: add script to install asdf plugins

---
+++ src/.chezmoiscripts/run_once_203-install-asdf-plugins.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+main() {
+  echo 'Installing asdf plugins...'
+
+  local tool_versions installed_plugins
+  tool_versions=$(cat ~/.tool-versions)
+  installed_plugins=$(asdf plugin list)
+
+  # tool_versions を改行区切りで while ループで回す
+  while IFS= read -r line; do
+    local plugin version
+    plugin=$(echo "$line" | cut -d ' ' -f 1)
+    version=$(echo "$line" | cut -d ' ' -f 2)
+
+    if echo "$installed_plugins" | grep -q "$plugin"; then
+      echo "Plugin '$plugin' is already installed."
+    else
+      echo "Installing 'plugin:$plugin'"
+      asdf plugin add "$plugin"
+    fi
+
+    if asdf list "$plugin" | grep -q "$version"; then
+      echo "Version '$plugin:$version' is already installed."
+    else
+      echo "Installing '$plugin:$version'"
+      asdf install "$plugin" "$version"
+    fi
+  done <<< "$tool_versions"
+}
+
+main
