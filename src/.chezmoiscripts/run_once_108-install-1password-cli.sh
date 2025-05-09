e5e8ffa15abaaa04708c4336549d92a619e26bae
r-okm <65703649+r-okm@users.noreply.github.com>
2025-05-09 16:27:15 +0900

    script: add 1password cli installation script

---
+++ src/.chezmoiscripts/run_once_108-install-1password-cli.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+main() {
+  echo 'Add the key for the 1Password apt repository:'
+  curl -sS https://downloads.1password.com/linux/keys/1password.asc |
+    sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+
+  echo 'Add the 1Password apt repository:'
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
+    sudo tee /etc/apt/sources.list.d/1password.list
+
+  echo 'Add the debsig-verify policy:'
+  sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+  curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol |
+    sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol &&
+    sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22 &&
+    curl -sS https://downloads.1password.com/linux/keys/1password.asc |
+    sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+
+  echo 'Install 1Password CLI:'
+  sudo apt update && sudo apt install 1password-cli
+
+  echo 'Check that 1Password CLI installed successfully:'
+  op --version
+}
+
+main
