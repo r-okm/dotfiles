cb9e150dec39d23f939baa79eaee6257d87f1ff0
r-okm <65703649+r-okm@users.noreply.github.com>
2025-10-23 18:08:52 +0900

    zsh: remove windows terminal integration code
    - remove `windows_terminal_osc_9_9` function
    - remove `windows_terminal_tab_title` function
    - remove WSL-specific hooks for Windows Terminal
    - no longer using Windows Terminal

--- src/private_dot_config/zsh/sync/precmd.zsh
+++ src/private_dot_config/zsh/sync/precmd.zsh
@@ -1,20 +1,3 @@
-windows_terminal_osc_9_9() {
-  # Inform Terminal about shell current working directory
-  # see: https://github.com/microsoft/terminal/issues/8166
-  printf '\e]9;9;%s\e\' "$(wslpath -w "$(pwd)")"
-}
-
-windows_terminal_tab_title() {
-  local current_dir=$(basename $PWD)
-  echo -ne '\033]0;'"[$WSL_DISTRO_NAME] $current_dir"'\a'
-}
-
-if [[ -n "$WSL_DISTRO_NAME" ]] && [[ -x "$(command -v wslpath)" ]]; then
-  autoload -Uz add-zsh-hook
-  add-zsh-hook precmd windows_terminal_osc_9_9
-  add-zsh-hook precmd windows_terminal_tab_title
-fi
-
 SUCCESS=$'\n[%F{magenta}%D{%H:%M:%S}%f] %F{yellow}%~%f [%F{reset}%?%f]\n%F{green}❯%f '
 FAILURE=$'\n[%F{magenta}%D{%H:%M:%S}%f] %F{yellow}%~%f [%F{red}%?%f]\n%F{red}❯%f '
 PROMPT=%(?.$SUCCESS.$FAILURE)
