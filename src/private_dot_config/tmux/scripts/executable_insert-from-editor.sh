5c0425e8225e1bcb14ddd5152fb69bb8de9d78d9
r-okm <65703649+r-okm@users.noreply.github.com>
2026-02-14 15:55:31 +0900

    nvim: standardize config file naming convention
    - Rename `edit_command_line.lua` to `init.edit_command_line.lua`
    - Rename `git_editor.lua` to `init.git_editor.lua`
    - Rename `tmux_edit.lua` to `init.tmux_edit.lua`
    - Update all references in `.profile`, git config, and tmux scripts
    - Establish consistent `init.*` prefix pattern for specialized nvim configs

--- src/private_dot_config/tmux/scripts/executable_insert-from-editor.sh
+++ src/private_dot_config/tmux/scripts/executable_insert-from-editor.sh
@@ -7,7 +7,7 @@ pane_path="$2"
 tmux set-environment TMUX_EDIT_PANE "$pane_id"
 tmux split-window -v -l 15 -c "$pane_path" \
   'tmpfile=$(mktemp /tmp/tmux-edit-XXXXXX) && \
-   nvim -u "$XDG_CONFIG_HOME/nvim/tmux_edit.lua" "$tmpfile" && \
+   nvim -u "$XDG_CONFIG_HOME/nvim/init.tmux_edit.lua" "$tmpfile" && \
    if [ -s "$tmpfile" ]; then \
      target=$(tmux show-environment TMUX_EDIT_PANE | cut -d= -f2) && \
      tmux load-buffer "$tmpfile" && tmux paste-buffer -t "$target" -r; \
